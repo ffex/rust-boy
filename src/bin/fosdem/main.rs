4d228258c3c607ef6c144a49444fad7e836d9a06
ffex <ffe.bassini@gmail.com>
2026-01-21 11:04:50 +0100

    added rust fosdem example

---
+++ src/bin/fosdem/main.rs
@@ -0,0 +1,24 @@
+use rust_boy::{
+    gb_std::inputs::PadButton,
+    rust_boy::{AnimationType, InputManager, RustBoy, TileSource},
+};
+
+fn main() {
+    let mut gb = RustBoy::new();
+
+    // Add sprite with coin animation tiles (7 frames: 0-6)
+    let coin = gb.add_sprite("Coin", TileSource::from_file("coin.2bpp"), 80, 72, 0);
+
+    // Add looping animation with relative frame indices 0 to 6
+    gb.sprites
+        .add_animation(coin, "CoinAnim", 0, 6, AnimationType::Loop);
+    gb.sprites.disable_animation("CoinAnim");
+    // Input handling
+    let mut inputs = InputManager::new();
+    inputs.on_press(PadButton::A, gb.sprites.enable_animation("CoinAnim"));
+    inputs.on_press(PadButton::B, gb.sprites.disable_animation("CoinAnim"));
+    inputs.on_press(PadButton::Left, gb.sprites.move_left_limit(coin, 1, 0));
+    inputs.on_press(PadButton::Right, gb.sprites.move_right_limit(coin, 1, 150));
+    gb.add_inputs(inputs);
+    println!("{}", gb.build());
+}
